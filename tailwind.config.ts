b90054c73cbf52458e3d297b4eea268094df573b
Krysto <quy.p.hoang@gmail.com>
2024-01-06 11:38:45 +0700

    apply body background

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,6 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
@@ -24,7 +17,7 @@ const config: Config = {
       almostBlack: 'rgb(54,54,54)',
       gray: 'rgb(176,176,176)',
       lightGray: 'rgb(245,245,245)',
-    }
+    },
   },
   plugins: [],
 }
