ed339684826a80e7c7a94f0890b18f2753d3b397
Krysto <quy.p.hoang@gmail.com>
2024-01-06 11:09:42 +0700

    colors config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,17 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: 'rgb(255,255,255)',
+      black: 'rgb(0,0,0)',
+      blue: 'rgb(39,100,217)',
+      darkBlue: 'rgb(39,32,71)',
+      almostBlack: 'rgb(54,54,54)',
+      gray: 'rgb(176,176,176)',
+      lightGray: 'rgb(245,245,245)',
+    }
   },
   plugins: [],
 }
