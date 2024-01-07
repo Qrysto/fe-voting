f98c14b64d2770cad5e6f77130c0769d19733998
Krysto <quy.p.hoang@gmail.com>
2024-01-07 23:30:44 +0700

    functional DigitInputs

--- src/app/vote/EnterPhone.tsx
+++ src/app/vote/EnterPhone.tsx
@@ -1,9 +1,51 @@
+'use client';
+
+import { useState, useRef } from 'react';
+import DigitInput from '@/components/DigitInput';
+
+const digitCount = 10;
+
+function PhoneInput() {
+  const [digits, setDigits] = useState(Array(digitCount).fill(''));
+  const inputDivs: React.MutableRefObject<Array<HTMLDivElement>> = useRef(
+    Array(digitCount).fill(null)
+  );
+
+  return (
+    <div className="flex items-center justify-between">
+      {digits.map((digit, i) => (
+        <DigitInput
+          key={i}
+          passRef={(el) => {
+            inputDivs.current[i] = el;
+          }}
+          value={digit}
+          setValue={(value) => {
+            const newDigits = [...digits];
+            newDigits.splice(i, 1, value);
+            setDigits(newDigits);
+            if (value) {
+              if (i < digitCount - 1) {
+                inputDivs.current[i + 1].focus();
+              } else {
+                inputDivs.current[i + 1].blur();
+              }
+            }
+          }}
+          className="text-5xl"
+        />
+      ))}
+    </div>
+  );
+}
+
 export default function EnterPhone() {
   return (
     <div>
       <h2 className="px-8 text-center text-2xl uppercase">
         Please enter your mobile phone number below
       </h2>
+      <PhoneInput />
     </div>
   );
 }
--- src/components/DigitInput.tsx
+++ src/components/DigitInput.tsx
@@ -1,27 +1,27 @@
 import { oswald } from '@/app/fonts';
 
-const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const validDigits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 export default function DigitInput({
   className,
   value,
   setValue,
   error,
-  ref,
+  passRef,
   ...rest
 }: {
   className?: string;
   value: string;
   setValue: (value: string) => void;
   error?: boolean;
-  ref: (el: HTMLDivElement) => void;
+  passRef: (el: HTMLDivElement) => void;
 }) {
   return (
     <div
-      ref={ref}
+      ref={passRef}
       tabIndex={0}
       onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (digits.includes(e.key)) {
+        if (validDigits.includes(e.key)) {
           setValue(e.key);
         } else if (
           e.key === 'Backspace' ||
@@ -33,9 +33,9 @@ export default function DigitInput({
       }}
       className={`flex h-[1.47em] w-[0.67em] items-center justify-center rounded-[3px] border-2 border-solid ${
         error ? 'border-red bg-red/5' : value ? 'border-green' : 'border-gray'
-      }  focus:border-blue ${oswald.className} font-medium ${className}  ${
-        error ? 'text-red' : 'text-darkBlue'
-      }`}
+      }  outline-none focus:border-blue ${
+        oswald.className
+      } font-medium ${className}  ${error ? 'text-red' : 'text-darkBlue'}`}
       {...rest}
     >
       {value}
