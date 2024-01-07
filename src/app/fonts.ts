3c1d553ad9d0c679bdfd249d640e4a4978377ca5
Krysto <quy.p.hoang@gmail.com>
2024-01-07 14:42:07 +0700

    set font variables and default titles font family

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -2,8 +2,10 @@ import { Oswald, Montserrat } from 'next/font/google';
 
 export const oswald = Oswald({
   subsets: ['latin'],
+  variable: '--font-oswald',
 });
 
 export const montserrat = Montserrat({
   subsets: ['latin'],
+  variable: '--font-montserrat',
 });
