76742b5f237d322306f75dde8ad11d03b18d3c5d
Krysto <quy.p.hoang@gmail.com>
2024-01-08 14:45:11 +0700

    BigButton disabled state

--- src/components/BigButton.tsx
+++ src/components/BigButton.tsx
@@ -4,17 +4,23 @@ export default function BigButton({
   primary,
   children,
   className,
+  disabled = false,
 }: {
   primary?: boolean;
   children?: React.ReactNode;
   className?: string;
+  disabled?: boolean;
 }) {
   return (
     <button
       className={`block h-12 w-full rounded-md ${
         oswald.className
       } text-center font-bold uppercase ${
-        primary ? 'bg-blue text-white' : 'bg-lightBlue text-blue'
+        disabled
+          ? 'bg-gray text-white'
+          : primary
+            ? 'bg-blue text-white'
+            : 'bg-lightBlue text-blue'
       } ${className || ''}`}
     >
       {children}
