387a9e067d7d24f43ff56676cf38c17a173f07ac
Krysto <quy.p.hoang@gmail.com>
2024-01-15 15:57:55 +0700

    added candidate headshots

---
+++ src/components/CandidateImage.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import Image from 'next/image';
+import { Candidate } from '@/types';
+import defaultAvatar from '@/default-avatar.jpg';
+
+export default function CandidateImage({
+  candidate,
+  className,
+}: {
+  candidate: Candidate;
+  className: string;
+}) {
+  const [error, setError] = useState(false);
+  const src = `/${candidate.Name.toUpperCase().replaceAll(' ', '_')}.jpg`;
+
+  return (
+    <Image
+      width={40}
+      height={40}
+      alt={candidate.Name}
+      className={'rounded-md ' + className}
+      src={error ? defaultAvatar : src}
+      onError={() => {
+        setError(true);
+      }}
+    />
+  );
+}
