53d617c3ec858cb8bc717a58f3d288535bf7a14e
Krysto <quy.p.hoang@gmail.com>
2024-01-28 08:51:41 +0700

    added link to the success message

--- src/app/vote/CastVote/VoteConfirmedModal.tsx
+++ src/app/vote/CastVote/VoteConfirmedModal.tsx
@@ -18,7 +18,19 @@ export default function VoteConfirmedModal({
         </div>
         <h1 className="text-4xl uppercase">Vote confirmed</h1>
         <p className="mt-[10px]">
-          Your vote has been submitted, thank you for your vote!
+          Your vote has been submitted, thank you for your vote! Your voice
+          matters.
+        </p>
+        <p className="mt-[10px]">
+          Help us fund the Free and Equal Debate and reduce corporate influence
+          in politics. Every donation counts. Support us here:{' '}
+          <a
+            href="https://givebutter.com/free-and-equal-debate"
+            target="_blank"
+            className="underline underline-offset-1"
+          >
+            https://givebutter.com/free-and-equal-debate
+          </a>
         </p>
         <BigButton className="mt-8" primary onClick={close}>
           Close & continue
