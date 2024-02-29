9b6b0803f05401a67fab0b47bc6732a1f44f1686
Krysto <quy.p.hoang@gmail.com>
2024-02-29 22:54:33 +0700

    added recalc interval message

--- src/app/result/poll2/UpdatedTime.tsx
+++ src/app/result/poll2/UpdatedTime.tsx
@@ -8,8 +8,13 @@ TimeAgo.addDefaultLocale(en);
 
 export default function UpdatedTime({ timeStamp }: { timeStamp: number }) {
   return (
-    <div className="mt-2 italic">
-      Updated <ReactTimeAgo date={timeStamp} locale="en-US" />
-    </div>
+    <>
+      <div className="mt-2 font-bold">
+        Updated <ReactTimeAgo date={timeStamp} locale="en-US" />.
+      </div>
+      <div className="mt-1">
+        Poll result will be automatically re-calculated every 10 minutes.
+      </div>
+    </>
   );
 }
