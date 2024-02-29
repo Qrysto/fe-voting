e6900894cac79061481815386ceebe6fcf5d6947
Krysto <quy.p.hoang@gmail.com>
2024-02-29 23:47:32 +0700

    set production constants

--- src/constants.ts
+++ src/constants.ts
@@ -5,8 +5,8 @@ export const codeDigitCount = 6;
 export const maxChoices = 5;
 
 export const tokenAddress =
-  '8CxPoCNnSRC9ALXAvTZBwPSuG2vWJZYhPvihUSfoJKgypP6ddDQ';
+  '8DkBdZWSRuaLHtNxaxxm5AU8jTL9Mm5hDJw9LvrLnP1zrKC62bZ';
 
-export const phoneNumbersTable = 'phonePoll2Test';
+export const phoneNumbersTable = 'phonePoll2';
 
-export const rcvResultKVKey = 'rcvResult';
+export const rcvResultKVKey = 'rcvResultPoll2';
