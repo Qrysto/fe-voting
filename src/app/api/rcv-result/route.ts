3182760adfae7a9c322af9cc26031540a4a049f7
Krysto <quy.p.hoang@gmail.com>
2024-02-29 16:38:19 +0700

    GET no body

--- src/app/api/rcv-result/route.ts
+++ src/app/api/rcv-result/route.ts
@@ -1,9 +1,6 @@
 import { rcvTable, rcvKey } from '@/constants';
 
 export async function GET() {
-  const body = JSON.stringify({
-    table: rcvTable,
-  });
   const res = await fetch(
     `http://node5.nexus.io:7080/local/list/records?table=${rcvTable}`,
     {
@@ -12,7 +9,6 @@ export async function GET() {
         Authorization: `Basic ${process.env.API_BASIC_AUTH}`,
         'Content-Type': 'application/json',
       },
-      body,
     }
   );
   if (!res.ok) {
