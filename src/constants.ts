6737a9fb8ae49553b345dc87b32b257431d2e26a
Krysto <quy.p.hoang@gmail.com>
2024-03-01 09:41:44 +0700

    updated token address

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export const codeDigitCount = 6;
 export const maxChoices = 5;
 
 export const tokenAddress =
-  '8DkBdZWSRuaLHtNxaxxm5AU8jTL9Mm5hDJw9LvrLnP1zrKC62bZ';
+  '8EorF6GG8XypDHPfDudYQR4RZbqxeJUSdtgdVzvsZfUBY76SKp6';
 
 export const phoneNumbersTable = 'phonePoll2';
 
