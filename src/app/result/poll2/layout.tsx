8fe42e4615957839385b564f44938e2651d88598
Krysto <quy.p.hoang@gmail.com>
2024-03-03 10:32:02 +0700

    fixed a minor UI bug

--- src/app/result/poll2/layout.tsx
+++ src/app/result/poll2/layout.tsx
@@ -8,12 +8,11 @@ export default function VoteLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div
-      className=" mx-[-1rem] h-full px-4"
-      style={{ background: 'rgb(243, 244, 248)' }}
-    >
-      <Header />
-      <main>{children}</main>
+    <div className="mx-[-1rem] h-full">
+      <div className="px-4" style={{ background: 'rgb(243, 244, 248)' }}>
+        <Header />
+        <main>{children}</main>
+      </div>
     </div>
   );
 }
