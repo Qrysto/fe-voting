c6fb139ab77daabc72b220644a9917fe150c3514
Krysto <quy.p.hoang@gmail.com>
2024-07-02 15:58:37 +0700

    wrapped modal in a container

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -14,14 +14,16 @@ export default function Modal({
 
   return (
     <div className="fixed bottom-0 left-0 right-0 top-0 bg-black/70">
-      <div className="absolute inset-x-5 inset-y-9 rounded-[28px] bg-white">
-        <div>{children}</div>
-        <button
-          className="absolute left-6 top-7 px-0 py-0 text-4xl"
-          onClick={close}
-        >
-          <Image src={closeIcon} alt="" />
-        </button>
+      <div className="container relative h-full overflow-y-auto md:max-w-3xl">
+        <div className="absolute inset-x-5 inset-y-9 rounded-[28px] bg-white">
+          <div>{children}</div>
+          <button
+            className="absolute left-6 top-7 px-0 py-0 text-4xl"
+            onClick={close}
+          >
+            <Image src={closeIcon} alt="" />
+          </button>
+        </div>
       </div>
     </div>
   );
