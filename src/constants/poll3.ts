c8017f256aebf0946650aa49bd68b59a0289c4ab
Krysto <quy.p.hoang@gmail.com>
2024-07-11 21:14:44 +0700

    added mainnet poll3 config

---
+++ src/constants/poll3.ts
@@ -0,0 +1,16 @@
+export const maxChoices = 7;
+
+export const ticker = 'poll3';
+
+export const tokenAddress =
+  '8EMkAeF1h1D74HrdpypKQKxNMjbMQ6UThtooM9J3QerERuHVVG1';
+
+export const phoneNumbersTable = 'poll3PhoneNumbers';
+
+export const allVotesKVKey = 'poll3AllVotes';
+
+export const rcvResultKVKey = 'poll3RCVResult';
+
+export const endTime = 1721620740000; // 2024 July 21st, 11:59pm EST
+
+export const endTimeString = '11:59PM on July 21st, 2024 EST';
