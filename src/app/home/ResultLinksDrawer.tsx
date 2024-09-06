b60d07a96dfd62736bca26115c95a4a0b0874404
Krysto <quy.p.hoang@gmail.com>
2024-09-06 09:01:36 +0700

    escaped html entities

--- src/app/home/ResultLinksDrawer.tsx
+++ src/app/home/ResultLinksDrawer.tsx
@@ -20,14 +20,14 @@ export default function ResultLinksDrawer() {
           variant="link"
           className={`${oswald.className} mt-4 text-sm uppercase text-lightBlue/90 underline underline-offset-2 active:text-lightBlue/80`}
         >
-          See all polls' results
+          See all polls&apos; results
         </Button>
       </DrawerTrigger>
       <DrawerContent className="max-h-[60%] after:hidden">
         <div className="container flex min-h-0 flex-1 flex-col md:max-w-3xl">
           <DrawerHeader className="flex-shrink-0">
             <DrawerTitle className="text-3xl uppercase text-darkBlue">
-              Polls' results
+              Polls&apos; results
             </DrawerTitle>
           </DrawerHeader>
           <div className="mx-[-1.5rem] flex-1 overflow-auto pb-10">
