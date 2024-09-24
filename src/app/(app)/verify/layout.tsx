109604a21e150b74816d6a1671971df33d8efd14
Krysto <quy.p.hoang@gmail.com>
2024-09-24 12:27:23 +0700

    verify page heading & description

--- src/app/(app)/verify/layout.tsx
+++ src/app/(app)/verify/layout.tsx
@@ -11,6 +11,8 @@ import {
   DrawerTrigger,
 } from '@/components/ui/drawer';
 import { ChevronDown } from 'lucide-react';
+import { Label } from '@/components/ui/label';
+import { ExternalLink } from '@/components/ui/typo';
 import allPolls from '@/constants/allPolls';
 import * as activePoll from '@/constants/activePoll';
 
@@ -28,6 +30,17 @@ export default function VerifyLayout({
 
   return (
     <div className="py-6">
+      <h1 className="mb-4 text-4xl">Verify</h1>
+      <p className="mb-4">
+        Free and Equal Voting app is powered by{' '}
+        <ExternalLink href="https://nexus.io/">Nexus blockchain</ExternalLink>,
+        meaning your votes are all kept on the blockchain for immutability and
+        verifiability.
+      </p>
+
+      <div className="mb-2">
+        <Label>Select the poll you want to verify</Label>
+      </div>
       <PollSelect />
 
       <Tabs
@@ -56,7 +69,7 @@ function PollSelect() {
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>
-        <div className="hover:bg-lightGra border-input bg-background hover:bg-accent hover:text-accent-foreground ring-offset-background focus-visible:ring-ring flex cursor-pointer items-center rounded-md border px-3 py-3 text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
+        <div className="flex cursor-pointer items-center rounded-md border border-border bg-popover px-3 py-3 text-sm font-medium ring-offset-background transition-colors hover:bg-popover/70 hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
           <div className="flex-1">
             <div className="text-sm opacity-75">{poll.pollTime}</div>
             <h3 className="text-xl text-darkBlue">{poll.pollName}</h3>
