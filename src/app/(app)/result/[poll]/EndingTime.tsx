4b68b830388ac4192095dd01ec43721ebabbea86
Krysto <quy.p.hoang@gmail.com>
2024-10-22 10:32:00 +0700

    EndingTime component auto-refreshing every minute or until the endTime is reached

--- src/app/(app)/result/[poll]/EndingTime.tsx
+++ src/app/(app)/result/[poll]/EndingTime.tsx
@@ -1,19 +1,34 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { formatDistanceToNow } from 'date-fns';
 
 export default function EndingTime({ endTime }: { endTime: number }) {
   const pollEnded = Date.now() > endTime;
+
+  // Refreshes the component every minute or until the endTime is reached
+  const [, setNow] = useState(Date.now());
+  useEffect(() => {
+    let timerId: NodeJS.Timeout | undefined = undefined;
+    const refresh = () => {
+      const now = Date.now();
+      // Set a state to manually rerender the component
+      setNow(now);
+
+      if (now > endTime) return;
+      const ms = Math.min(endTime - now, 60000);
+      timerId = setTimeout(refresh, ms);
+    };
+    refresh();
+    return () => clearTimeout(timerId);
+  }, []);
 
   return (
     <div className="mt-1">
       {pollEnded ? (
         'This poll has ended'
       ) : (
-        <span>
-          This poll will end in
-          {formatDistanceToNow(endTime, { includeSeconds: true })}
-        </span>
+        <span>This poll will end in {formatDistanceToNow(endTime, {})}</span>
       )}
     </div>
   );
